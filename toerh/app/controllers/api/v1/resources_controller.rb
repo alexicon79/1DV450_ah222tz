1cfeeeecdc52509080b23acd2d2a8ca3925f1fdd
Alexander Hall <ah@alxhall.se>
2014-02-11 23:06:11 +0100

    Added limit and offset filtering

--- toerh/app/controllers/api/v1/resources_controller.rb
+++ toerh/app/controllers/api/v1/resources_controller.rb
@@ -4,13 +4,6 @@ module Api
       #before_filter :restrict_access
       respond_to :json
 
-      # @@all = true
-      # @@user = false
-      # @@licence = false
-      # @@tag = false
-
-      # @@filter_true = "true"
-
       def index
         apply_filters
 
@@ -19,20 +12,6 @@ module Api
         else
           @resources = Resource.all
         end
-
-        # @filter_params_length = params.except(:filter, :action, :format, :controller).length
-
-
-        # case filter
-        #   when @@user
-        #     @resources = get_resources_by_user
-        #   when @@licence
-        #     @resources = get_resources_by_licence
-        #   when @@tag
-        #     @resources = get_resources_by_tag
-        #   when @@all
-        #     @resources = Resource.all
-        # end
 
       end
 
@@ -54,93 +33,6 @@ module Api
 
 
       private
-
-      # def apply_filters
-      #   if params[:filter].present? && params[:filter].downcase == @@filter_true
-      #     if params[:user].present?
-      #       @@user = true
-      #     end
-      #     if params[:licence].present?
-      #       @@licence = true
-      #     end
-      #     if params[:tag].present?
-      #       @@tag = true
-      #     end
-      #   else
-      #     @@all = true
-      #   end
-      # end
-
-      # def get_filtered_resources
-      #   if @@all
-      #     @resources = Resource.all
-      #   end
-
-      #   if @@user
-      #     if @resources.nil?
-      #       @resources = get_resources_by_user
-      #     else
-      #       @resources += get_resources_by_user
-      #     end
-      #   end
-
-      #   if @@licence
-      #     if @resources.nil?
-      #       @resources = get_resources_by_licence
-      #     else
-      #       @resources += get_resources_by_licence
-      #     end
-      #   end
-
-      #   if @@tag
-      #     if @resources.nil?
-      #       @resources = get_resources_by_tag
-      #     else
-      #       @resources += get_resources_by_tag
-      #     end
-      #   end
-
-      #   # TODO: filter resource shown as many times as params.length
-      #   # @filter_params_length
-
-      # end
-
-      # def get_resources_by_user
-      #   # return resources if param value is a number less than total amount of users
-      #   if params[:user].to_i <= User.all.count && params[:user].to_i != 0
-      #     return User.find(params[:user]).resources
-      #   else
-      #     return nil
-      #   end
-      # end
-
-      # def get_resources_by_licence
-      #   # return resources if param value is a number less than total amount of licences
-      #   if params[:licence].to_i <= Licence.all.count && params[:licence].to_i != 0
-      #     return Licence.find(params[:licence]).resources
-      #   elsif params[:licence].match(/^[a-z]+$/i)
-      #     return Licence.where("lower(licence_type) = ?", params[:licence].downcase).find_each do |licence|
-      #       return licence.resources
-      #     end
-      #   else
-      #     return nil
-      #   end
-      # end
-
-      # def get_resources_by_tag
-      #   # return resources by tag_id if param value is a number less than total amount of tags
-      #   if params[:tag].to_i <= Tag.all.count && params[:tag].to_i != 0
-      #     return Tag.find(params[:tag]).resources
-      #   # return resources by tag_name
-      #   # TODO: check if string is not numbers...
-      #   elsif params[:tag].match(/^[a-z]+$/i)
-      #     return Tag.where("lower(tag_name) = ?", params[:tag].downcase).find_each do |tag|
-      #       return tag.resources
-      #     end
-      #   else
-      #     return nil
-      #   end
-      # end
 
       def restrict_access
         authenticate_or_request_with_http_token do |token, options|
